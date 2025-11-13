fc1d0d0970c77e1bc345df7cb3217750ef069b93
Kamalov-Q <kamolovquvomiddin9@gmail.com>
2025-11-14 01:26:02 +0500

    initial commit auth service set up

--- services/auth-service/src/auth/auth.service.ts
+++ services/auth-service/src/auth/auth.service.ts
@@ -218,7 +218,7 @@ export class AuthService {
         });
         if (!user) {
             throw new UnauthorizedException('User not found');
-        }
+        } 
         return user;
     }
 }
